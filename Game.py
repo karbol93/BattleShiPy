06a78c732e8906cf6cf8844a415f5a632df2721e
krzplucinski <stg.krzysztof.plucinski@gmail.com>
2018-06-04 10:22:49 +0200

    Added simple gameloop in Game class

--- Game.py
+++ Game.py
@@ -1,2 +1,28 @@
+import pygame
+
 class Game:
-	pass
+	
+	def __init__(self):
+		self.exit = False
+		
+
+	def event_loop(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.exit = True
+		
+		
+	def update(self):
+		pass
+		
+		
+	def draw(self):
+		pygame.display.flip()
+		
+		
+	def run(self):
+		while not self.exit:
+			self.event_loop()
+			self.update()
+			self.draw()
+			
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, sys, os
+from Game import Game
 
 WINDOW_NAME = "BattleShiPy game"
 SCREEN_SIZE = (500, 500)
@@ -10,8 +11,5 @@ if __name__ == "__main__":
 	pygame.display.set_caption(WINDOW_NAME)
 	pygame.display.set_mode(SCREEN_SIZE)
 	
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit(0)
-		pygame.display.flip()
+	game = Game()
+	game.run()

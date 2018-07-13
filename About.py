8bb20a4008931e3b83897e4db199c7fe6cdd8a17
Sothus <krzysztof.plucinski95@gmail.com>
2018-07-13 11:29:18 +0200

    Added empty method into Options and About classes

--- About.py
+++ About.py
@@ -3,7 +3,16 @@ import pygame
 class About:
 
     def __init__(self):
+        self.done = False
+
+    def event_loop(self):
         pass
 
-    def run(self):
+    def draw(self):
         pass
+
+
+    def run(self):
+        while not self.done:
+            self.event_loop()
+            self.draw()
--- Options.py
+++ Options.py
@@ -1,9 +1,22 @@
 import pygame
 
 class Options:
+    WINDOW_PROPERTIES = {
+        "fullscreen": False,
+        "screen_resolution": (550, 500)
+    }
+
 
     def __init__(self):
+        self.done = False
+
+    def event_loop(self):
+        pass
+
+    def draw(self):
         pass
 
     def run(self):
-        pass
+        while not self.done:
+            self.event_loop()
+            self.draw()
